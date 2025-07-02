77aa3beb74a54256b3785d250bbeb2adaeb7469e
nckrtl <nick.retel@gmail.com>
2025-07-02 09:04:22 +0200

    wip

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-import { Button } from "@hardimpactdev/liftoff-ui";
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <Head title="Home" />
     <Link :href="Controllers.HomeController.show()">Home</Link>
-
-    <Button>Click me</Button>
 </template>
